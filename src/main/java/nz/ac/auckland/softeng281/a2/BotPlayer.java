082bd115dec5f82c8df30dfa9d6c9b534444bfe8
Ananya <aahl907@aucklanduni.ac.nz>
2021-04-14 15:22:37 +1200

    task 1 COMPLETED
    Fixed failures 7 and 8 in task 1. Wrote makeABet() method. Used Math.random function and double to integer conversion to generate a random bet value for bots

--- src/main/java/nz/ac/auckland/softeng281/a2/BotPlayer.java
+++ src/main/java/nz/ac/auckland/softeng281/a2/BotPlayer.java
@@ -13,7 +13,7 @@ public class BotPlayer extends Participant {
 
 	@Override
 	public Action decideAction() {
-		// TODO Task 1 - Failures 2 to 5, Wrote decideAction() method
+		// TODO Task 1 - Failures 2 to 6, Wrote decideAction() method
 		// Retrieves current hand using getCurrentHand method and counts the total score of that hand using getScore method.
 		Hand current = getCurrentHand();
 		int totalScore = current.getScore();
@@ -28,7 +28,15 @@ public class BotPlayer extends Participant {
 
 	@Override
 	public int makeABet() {
-		// TODO
-		return -1; // FIXME
+		// TODO Task 1 - Failures 7 and 8, wrote makeABet() method
+		
+		// Uses Math.Random function to find a random double between 0 and 99 
+		double random = (Math.random())*100;
+		
+		// Converts to double to integer and adds one to get range between 1 and 100.
+		int randomBet = (int)random + 1;
+		
+		
+		return randomBet;
 	}
 }
