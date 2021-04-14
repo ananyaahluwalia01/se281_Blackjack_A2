50b1b2074e40386a0bdb4e8e290e0e98b52fb3ed
Ananya <aahl907@aucklanduni.ac.nz>
2021-04-14 14:46:38 +1200

    task 1 failures 2 to 5 fixed
    Wrote decideAction() method,  called methods getCurrentHand and getScore. Also edited previous comment made on BlackJack.java to say failure 1 instead of task 1.

--- src/main/java/nz/ac/auckland/softeng281/a2/BotPlayer.java
+++ src/main/java/nz/ac/auckland/softeng281/a2/BotPlayer.java
@@ -13,8 +13,17 @@ public class BotPlayer extends Participant {
 
 	@Override
 	public Action decideAction() {
-		// TODO
-		return new Random().nextBoolean() ? Action.HIT : Action.HOLD; // FIXME
+		// TODO Task 1 - Failures 2 to 5, Wrote decideAction() method
+		// Retrieves current hand using getCurrentHand method and counts the total score of that hand using getScore method.
+		Hand current = getCurrentHand();
+		int totalScore = current.getScore();
+		
+		// Simple conditional statement checking whether to hold or hit.
+		if (totalScore < 17) {
+			return Action.HIT;
+		} else {
+			return Action.HOLD; 
+		}
 	}
 
 	@Override
